6e37f0690f166ab205df9de2e611d77ecb175a2c
Justin Millar <justinjmillar@gmail.com>
2018-03-29 16:20:28 -0400

    cleaning up

--- r/gs_data.R
+++ r/gs_data.R
@@ -38,29 +38,57 @@ ggsave("img/attendance.png", plotAttend)
 unique(na.omit(record$presenter))
 
 # Spring 2018 Sign-in data ----
-first_week
+first_week <- gs_title("Supp Data - UF R Meetup Sign-In") %>% 
+  gs_read(ws = 1) %>% 
+  janitor::clean_names() %>% 
+  mutate(date = as_date(date)) %>% 
+  select(date, position, department)
 
-# Departmental attendence
 s18_record <- gs_title("UF R Meetup Sign-In (Responses)") %>% 
   gs_read(ws = 1) %>% 
   janitor::clean_names() %>% 
   mutate(timestamp = lubridate::mdy_hms(timestamp)) %>% 
-  mutate(date = as_date(timestamp),
-         department = abbreviate(department)) %>% 
-  select(date, position, department)
+  mutate(date = as_date(timestamp)) %>% 
+  select(date, position, department) %>% 
+  rbind(first_week)
 
-plotDep <- ggplot(s18_record, aes(x = department, fill = department)) +
+# Departmental attendance
+unique(na.omit(s18_record$department))
+
+s18_dep <- s18_record %>% 
+  mutate(department = abbreviate(department, 8))
+
+plotDep <- ggplot(s18_dep, aes(x = department, fill = department)) +
   geom_bar() +
-  #geom_text()
   coord_polar() +
   theme_minimal() +
-  #margin(t = 0, r = 0, b = 0, l = 0, unit = "pt") +
+  ggtitle("Spring 2018 Attendance by Department") +
   theme(legend.position = "none", 
         axis.line=element_blank(),
         plot.margin = margin(0,0,0,0, "cm"),
-        #axis.text.x=element_blank(),
+        axis.text.x=element_text(size = 6, color = "black"),
         #axis.text.y=element_blank(),
         axis.title.x=element_blank(),
         axis.title.y=element_blank())
 plotDep
 ggsave("img/s18-department.png", plotDep)
+
+# Positional attendance
+s18_pos <- s18_record %>% 
+  select(date, position) %>% 
+  mutate(position = abbreviate(position, 12)) %>% 
+  na.omit() %>% 
+  group_by(position) %>% 
+  summarize(n = n()) %>% 
+  mutate(freq = n/sum(n))
+
+plotPos <- ggplot(s18_pos, aes(x = position, y = freq, fill = position)) +
+  geom_col() + 
+  scale_y_continuous(labels = percent) +
+  xlab("") + ylab("Attendance") + 
+  ggtitle("Spring 2018 Attendance by Position") +
+  theme_classic() +
+  theme(legend.position = "none", 
+        axis.text.x = element_text(angle = 30, hjust = 1))
+plotPos
+ggsave("img/s18-position.png", plotPos)
