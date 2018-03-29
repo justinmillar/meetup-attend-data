71833b84243fc6b52f183b3c27a5dbfcef164fc1
Justin Millar <justinjmillar@gmail.com>
2018-03-29 12:15:28 -0400

    updating figures from google sheets

---
+++ r/gs_data.R
@@ -0,0 +1,62 @@
+library(googlesheets)
+library(lubridate)
+library(tidyverse)
+
+# Running record of attendance ----
+record <- gs_title("UF R Meetup Record") %>% 
+  gs_read(ws = 1) %>% 
+  janitor::clean_names() %>% 
+  mutate(date = as_date(date))
+
+# Total attendance
+record %>%  
+  mutate(num = pmax(attendence, head_count, na.rm = T)) %>%
+  select(num) %>% 
+  na.omit() %>%
+  summarise_all(sum)
+
+# Attendance by week for each semester
+df <- record %>%  
+  mutate(num = pmax(attendence, head_count, na.rm = T)) %>% 
+  select(date, sm, wk, num) %>% 
+  na.omit() %>% 
+  group_by(sm, wk) %>% 
+  summarize(num = sum(num))
+
+plotAttend <- ggplot(df, aes(x = wk, y = num, color = sm)) +
+  geom_line(size = 2) +
+  geom_point(size = 3, shape = 21, color = "black", aes(fill = sm)) +
+  scale_color_manual(values = c("#009688", "#FF6F00", "#9E9E9E")) +
+  scale_fill_manual(values = c("#009688", "#FF6F00", "#9E9E9E")) +
+  xlab("Week") + ylab("Attendance") +
+  theme_classic() +
+  theme(legend.position = "top")
+
+ggsave("img/attendance.png", plotAttend)
+
+# Unique Presenters
+unique(na.omit(record$presenter))
+
+# Spring 2018 Sign-in data ----
+first_week
+
+# Departmental attendence
+s18_record <- gs_title("UF R Meetup Sign-In (Responses)") %>% 
+  gs_read(ws = 1) %>% 
+  janitor::clean_names() %>% 
+  mutate(timestamp = lubridate::mdy_hms(timestamp)) %>% 
+  mutate(date = as_date(timestamp)) %>% 
+  select(date, position, department)
+
+plotDep <- ggplot(s18_record, aes(x = department, fill = department)) +
+  geom_bar(aes(label = department)) +
+  #geom_text()
+  coord_polar() +
+  theme_minimal() +
+  theme(legend.position = "none", 
+        axis.line=element_blank(),
+        #axis.text.x=element_blank(),
+        #axis.text.y=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank())
+ggsave("img/s18-department.png", plotDep)
